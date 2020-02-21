9c8edc159016d2b14a6ba678d37144337f21f33e
carlosivis <carlosivis1@gmail.com>
2020-02-20 22:45:26 -0300

    Response adicionado

--- app/src/main/java/br/com/example/maratonasamsung/model/Requests/DoencasRequest.kt
+++ app/src/main/java/br/com/example/maratonasamsung/model/Requests/DoencasRequest.kt
@@ -2,6 +2,6 @@ package br.com.example.maratonasamsung.model.Requests
 
 import java.io.Serializable
 
-data class Doencas(
+data class DoencasRequest(
     val nome: String
 ):Serializable
---
+++ app/src/main/java/br/com/example/maratonasamsung/model/Responses/DoencasResponse.kt
@@ -0,0 +1,7 @@
+package br.com.example.maratonasamsung.model.Responses
+
+data class DoencasResponse(
+    val nome: String,
+    val sintoma: String,
+    val prevencao: String
+)
