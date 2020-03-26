92c7f514ea5a7dc2c9af3c056d438c036718d604
Taís Christofani <taiskc@gmail.com>
2020-03-26 12:40:44 -0300

    novas cores, botoes arredondados e toolbar configurada

--- app/src/main/java/br/com/example/maratonasamsung/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/example/maratonasamsung/ui/main/MainActivity.kt
@@ -1,23 +1,29 @@
 package br.com.example.maratonasamsung.ui.main
 
-import android.app.Activity
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import br.com.example.maratonasamsung.R
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var sharedPreferences: SharedPreferences
     lateinit var editor: SharedPreferences.Editor
-    lateinit var activity: Activity
-    val firstRun = "Não entendo"
+    private val firstRun = "Não entendo"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         sharedPreferences = getSharedPreferences("com.MaratonaSamsung", MODE_PRIVATE)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
     }
 
     override fun onResume() {
@@ -26,6 +32,25 @@ class MainActivity : AppCompatActivity() {
             editor = sharedPreferences.edit()
             editor.putBoolean(firstRun, false).commit()
             startActivity(Intent(this, TutorialActivity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuTutorial -> {
+                startActivity(Intent(this, TutorialActivity::class.java))
+                return true
+            }
+            R.id.menuConfiguracao -> {
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
