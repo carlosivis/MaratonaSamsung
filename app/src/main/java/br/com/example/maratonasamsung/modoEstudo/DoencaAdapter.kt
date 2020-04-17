a664313a8f9fdaf38c9bf437fa7a32fb529fa17f
carlosivis <carlosivis1@gmail.com>
2020-04-17 13:00:13 -0300

    Acesso a tela de detalhes da doença feito(ainda falta correcao)

--- app/src/main/java/br/com/example/maratonasamsung/modoEstudo/DoencaAdapter.kt
+++ app/src/main/java/br/com/example/maratonasamsung/modoEstudo/DoencaAdapter.kt
@@ -1,15 +1,20 @@
 package br.com.example.maratonasamsung.modoEstudo
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import br.com.example.maratonasamsung.R
 import br.com.example.maratonasamsung.model.Responses.DoencasResponse
 import kotlinx.android.synthetic.main.recycler_view_listadoencas.view.txtDoencaNomeLista
+import kotlinx.coroutines.joinAll
 
 class DoencaAdapter(
-    private val list: List<DoencasResponse>?, val itemClickListener: OnItemClickListener
-):  RecyclerView.Adapter<DoencaAdapter.DoencaViewHolder>() {
+    private val list: List<DoencasResponse>?):  RecyclerView.Adapter<DoencaAdapter.DoencaViewHolder>() {
 
     class DoencaViewHolder(inflater: LayoutInflater, parent: ViewGroup) : RecyclerView.ViewHolder(
         inflater.inflate(
@@ -18,15 +23,15 @@ class DoencaAdapter(
             false)
     ) {
 
-        fun bind(doencas: DoencasResponse, clickListener: OnItemClickListener) {
+        fun bind(doencas: DoencasResponse) {
+            Log.d("Self Doenca :", doencas.toString())
             itemView.txtDoencaNomeLista?.text = doencas.nome
-//            itemView.txtArraySintoma?.text = doencas.sintomas.joinToString("\n") { it.nome }
-//            itemView.txtPrevencao?.text = doencas.prevencao.joinToString("\n") { it.nome }
-//            itemView.txtAgente?.text = "Agente: ${doencas.agente}"
-//            itemView.txtTipo?.text = "Tipo: ${doencas.tipo}"
-            itemView.setOnClickListener {
-                clickListener.onItemClicked(doencas)
-            }
+                itemView.txtDoencaNomeLista.setOnClickListener{
+                    val parametros =  Bundle()
+                    parametros.putString("selfDoenca", doencas.nome)
+                    Navigation.createNavigateOnClickListener(R.id.action_chooseFragment_to_itemChooseFragment
+                    , parametros)
+                    .onClick(itemView)}
         }
     }
 
@@ -38,13 +43,10 @@ class DoencaAdapter(
     override fun onBindViewHolder(holder: DoencaViewHolder, position: Int) {
 
         val doenca: DoencasResponse = list?.get(position)!!
-        holder.bind(doenca,itemClickListener)
-
+        holder.bind(doenca)
     }
 
     override fun getItemCount(): Int = list!!.size
 
-    interface OnItemClickListener{
-        fun onItemClicked(doencas: DoencasResponse)
-    }
+
 }
