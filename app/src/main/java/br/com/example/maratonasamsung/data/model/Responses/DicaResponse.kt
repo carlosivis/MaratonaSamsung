85dab07b2f4d9b1152c944fb8c2fdcb3baac91b2
carlosivis <carlosivis1@gmail.com>
2020-05-05 18:24:43 -0300

    bugfix crash ao iniciar

--- app/src/main/java/br/com/example/maratonasamsung/data/model/Responses/DicaResponse.kt
+++ app/src/main/java/br/com/example/maratonasamsung/data/model/Responses/DicaResponse.kt
@@ -1,5 +1,9 @@
 package br.com.example.maratonasamsung.data.model.Responses
 
+import br.com.example.maratonasamsung.model.Responses.Prevencao
+import br.com.example.maratonasamsung.model.Responses.Sintoma
+import br.com.example.maratonasamsung.model.Responses.Transmicao
+
 data class DicaResponse(
     val prevencoes: List<Prevencao>,
     val sintomas: List<Sintoma>,
