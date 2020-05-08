2cb5ed6edf28d66eed7c250c4c550a828ccd2156
carlosivis <carlosivis1@gmail.com>
2020-05-08 16:40:33 -0300

    update

---
+++ app/src/main/java/br/com/example/maratonasamsung/PlaceholderRodadaFragment.kt
@@ -0,0 +1,50 @@
+package br.com.example.maratonasamsung
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_placeholder_rodada.*
+import java.util.*
+import kotlin.concurrent.schedule
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PlaceholderRodadaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_placeholder_rodada, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val jogador = requireArguments().getString("jogador_nome")
+        val diqueiro = requireArguments().getString("diqueiro")
+        Log.d("Testezinho","$jogador - $diqueiro")
+        val parametros = Bundle()
+        parametros.getString("id_sessao")
+        parametros.putString("jogador_nome",jogador)
+        parametros.putInt("id_sessao",requireArguments().getInt("id_sessao"))
+        if (jogador==diqueiro)
+            txtTipoJogador.text = "Agora você será o Diqueiro"
+        else
+            txtTipoJogador.text = "Agora você será o Adivinhador"
+
+        Timer().schedule(5000) {
+            if (jogador == diqueiro)
+                Navigation.findNavController(view).navigate(R.id.action_placeholderRodadaFragment_to_roomDiqueiroDoencaFragment,parametros)
+
+            else
+                Navigation.findNavController(view).navigate(R.id.action_placeholderRodadaFragment_to_roomAdivinhadorFragment,parametros)
+        }
+
+    }
+}
