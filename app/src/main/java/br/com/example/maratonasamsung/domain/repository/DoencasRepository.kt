f2096ebea59e84d40581ada3ea87b17e034db68f
carlosivis <carlosivis1@gmail.com>
2020-05-15 01:51:35 -0300

    bugfix

--- app/src/main/java/br/com/example/maratonasamsung/domain/repository/DoencasRepository.kt
+++ app/src/main/java/br/com/example/maratonasamsung/domain/repository/DoencasRepository.kt
@@ -1,7 +1,7 @@
 package br.com.example.maratonasamsung.domain.repository
+import br.com.example.maratonasamsung.model.Responses.*
 
-import br.com.example.maratonasamsung.domain.model.DoencasResponse
 
 interface DoencasRepository {
-    suspend fun doencas(): List<br.com.example.maratonasamsung.model.Responses.DoencasResponse>
+    suspend fun doencas(): List<DoencasResponse>
 }
